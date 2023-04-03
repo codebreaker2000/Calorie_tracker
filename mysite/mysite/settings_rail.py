4603eacbc056df40ba0e061d0c069a0e8938ecec
Bharat <bharat1910109@akgec.ac.in>
2023-04-04 00:43:54 +0530

    agail retrying to allow host

--- mysite/mysite/settings_rail.py
+++ mysite/mysite/settings_rail.py
@@ -2,4 +2,4 @@ from mysite.settings import *
 from decouple import config
 SECRET_KEY=config('SECRET_KEY')
 
-ALLOWED_HOSTS = ['https://calorietracker-production.up.railway.app/']
+ALLOWED_HOSTS = ['calorietracker-production.up.railway.app']
